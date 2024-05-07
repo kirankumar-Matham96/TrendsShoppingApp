775e96c0875b15ef277d9533b9b1f329c159d3bc
kirankumar-Matham96 <mathamkirankumar96@gmail.com>
2024-05-07 17:52:02 +0530

    added: login instructions

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,58 +1,58 @@
-import {Component} from 'react'
-import Cookies from 'js-cookie'
-import {Redirect} from 'react-router-dom'
+import { Component } from "react";
+import Cookies from "js-cookie";
+import { Redirect } from "react-router-dom";
 
-import './index.css'
+import "./index.css";
 
 class LoginForm extends Component {
   state = {
-    username: '',
-    password: '',
+    username: "",
+    password: "",
     showSubmitError: false,
-    errorMsg: '',
-  }
+    errorMsg: "",
+  };
 
-  onChangeUsername = event => {
-    this.setState({username: event.target.value})
-  }
+  onChangeUsername = (event) => {
+    this.setState({ username: event.target.value });
+  };
 
-  onChangePassword = event => {
-    this.setState({password: event.target.value})
-  }
+  onChangePassword = (event) => {
+    this.setState({ password: event.target.value });
+  };
 
-  onSubmitSuccess = jwtToken => {
-    const {history} = this.props
+  onSubmitSuccess = (jwtToken) => {
+    const { history } = this.props;
 
-    Cookies.set('jwt_token', jwtToken, {
+    Cookies.set("jwt_token", jwtToken, {
       expires: 30,
-    })
-    history.replace('/')
-  }
+    });
+    history.replace("/");
+  };
 
-  onSubmitFailure = errorMsg => {
-    this.setState({showSubmitError: true, errorMsg})
-  }
+  onSubmitFailure = (errorMsg) => {
+    this.setState({ showSubmitError: true, errorMsg });
+  };
 
-  submitForm = async event => {
-    event.preventDefault()
-    const {username, password} = this.state
-    const userDetails = {username, password}
-    const url = 'https://apis.ccbp.in/login'
+  submitForm = async (event) => {
+    event.preventDefault();
+    const { username, password } = this.state;
+    const userDetails = { username, password };
+    const url = "https://apis.ccbp.in/login";
     const options = {
-      method: 'POST',
+      method: "POST",
       body: JSON.stringify(userDetails),
-    }
-    const response = await fetch(url, options)
-    const data = await response.json()
+    };
+    const response = await fetch(url, options);
+    const data = await response.json();
     if (response.ok === true) {
-      this.onSubmitSuccess(data.jwt_token)
+      this.onSubmitSuccess(data.jwt_token);
     } else {
-      this.onSubmitFailure(data.error_msg)
+      this.onSubmitFailure(data.error_msg);
     }
-  }
+  };
 
   renderPasswordField = () => {
-    const {password} = this.state
+    const { password } = this.state;
 
     return (
       <>
@@ -68,11 +68,11 @@ class LoginForm extends Component {
           placeholder="Password"
         />
       </>
-    )
-  }
+    );
+  };
 
   renderUsernameField = () => {
-    const {username} = this.state
+    const { username } = this.state;
 
     return (
       <>
@@ -88,15 +88,15 @@ class LoginForm extends Component {
           placeholder="Username"
         />
       </>
-    )
-  }
+    );
+  };
 
   render() {
-    const {showSubmitError, errorMsg} = this.state
-    const jwtToken = Cookies.get('jwt_token')
+    const { showSubmitError, errorMsg } = this.state;
+    const jwtToken = Cookies.get("jwt_token");
 
     if (jwtToken !== undefined) {
-      return <Redirect to="/" />
+      return <Redirect to="/" />;
     }
 
     return (
@@ -117,6 +117,9 @@ class LoginForm extends Component {
             className="login-website-logo-desktop-img"
             alt="website logo"
           />
+          <p class="text-red">Use below credentials to login*</p>
+          <span>Username: rahul</span>
+          <span>password: rahul@2021</span>
           <div className="input-container">{this.renderUsernameField()}</div>
           <div className="input-container">{this.renderPasswordField()}</div>
           <button type="submit" className="login-button">
@@ -125,8 +128,8 @@ class LoginForm extends Component {
           {showSubmitError && <p className="error-message">*{errorMsg}</p>}
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default LoginForm
+export default LoginForm;
